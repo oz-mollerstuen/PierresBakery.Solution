79ca438cbe2ba45e6952ecdfa1bbd1d17acacea6
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-12-16 10:37:06 -0800

    WIP: Add to vendor controllor. Early lunch, finish controller setup and start views on return

--- Controllers/VendorController.cs
+++ Controllers/VendorController.cs
@@ -1,5 +1,19 @@
-
 using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+  public class VendorController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+  }
+}
